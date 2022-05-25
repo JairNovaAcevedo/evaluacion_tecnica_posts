68bb217f86d220965c09108dd40335994063b37a
JairNovaAcevedo <104371954+JairNovaAcevedo@users.noreply.github.com>
2022-05-25 16:39:25 -0500

    IMPLEMEMTACIÓN - Creando la base del proyecto evaluación tecnica posts

---
+++ app/src/main/java/com/example/evaluacion_tecnica_post/viewInicio/PostActivity.kt
@@ -0,0 +1,12 @@
+package com.example.evaluacion_tecnica_post.viewInicio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.evaluacion_tecnica_post.R
+
+class PostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
